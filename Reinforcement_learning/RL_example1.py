9560a65541992c3326eafa09cd5d006a6aa21b73
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-05-28 21:31:45 +0800

    实例化环境，初始化动作和状态

--- Reinforcement_learning/RL_example1.py
+++ Reinforcement_learning/RL_example1.py
@@ -1,5 +1,6 @@
 # https://blog.csdn.net/qq_40041064/article/details/105047546
 
+from pickletools import optimize
 import tensorflow as tf 
 import numpy as np 
 import gym 
@@ -31,3 +32,24 @@ class QNetwork(tf.keras.Model):
     def predict(self, inputs):
         q_values = self(inputs)
         return tf.argmax(q_values, axis=1)
+
+if __name__ == '__main__':
+    env = gym.make('CartPole-v1')                           # 实例化一个游戏环境，参数为游戏名称
+    model = QNetwork()                                      # 实例化一个Q网络
+    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    replay_buffer = deque(maxlen=10000)                     # 使用一个 deque 作为经验回放池
+    epsilon = initial_epsilon
+    for episode_id in range(num_episodes):
+        state = env.reset()                                 # 初始化环境，获得初始状态
+        epsilon = max(                                      # 计算当前探索率
+            initial_epsilon * (num_exploration_episodes - episode_id) / num_exploration_episodes,
+            final_epsilon)
+        for t in range(max_len_episode):
+            env.render()                                    # 对当前帧进行渲染，绘图到屏幕
+            if random.random() < epsilon:                   # epsilon-greedy 探索策略，以 epsilon 的概率随机选择动作
+                action = env.action_space.sample()          # 选择随机动作（探索）
+            else:
+                action = model.predict(np.expand_dims(state, axis=0)).numpy()   # 选择模型计算出的 Q 值最大的动作
+                action = action[0]
+
+        
