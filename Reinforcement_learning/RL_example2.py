14295103bfac49757bb175dfc5ad57498598c6c2
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-06-02 21:18:14 +0800

    设定画图参数，加载库

--- Reinforcement_learning/RL_example2.py
+++ Reinforcement_learning/RL_example2.py
@@ -2,3 +2,15 @@ import gym,os
 import numpy as np 
 import matplotlib 
 from matplotlib import pyplot as plt
+
+matplotlib.rcParams['font.size'] = 18
+matplotlib.rcParams['figure.titlesize'] = 18
+matplotlib.rcParams['figure.figsize'] = [9, 7]
+matplotlib.rcParams['font.family'] = ['KaiTi']
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+import tensorflow as tf 
+from tensorflow import keras 
+from tensorflow.keras import layers,optimizers,losses 
+from PIL import Image
+
