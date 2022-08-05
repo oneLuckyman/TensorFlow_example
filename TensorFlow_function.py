435b94fc2364c0edaf43c3861fb5b50d1b15853c
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-08-05 22:12:01 +0800

    各种使用过的 TensorFlow 的内置函数

--- TensorFlow_function.py
+++ TensorFlow_function.py
@@ -6,10 +6,34 @@ import tensorflow as tf
 tf.argmax()         # 找到最大值并返回索引
 tf.reduce_max()     # 求最大值
 tf.reduce_mean()    # 求平均值
+tf.square()
 tf.reduce_sum()
 tf.one_hot()
+tf.cast()
+tf.keras.utils.to_categorical()
 
 tf.keras.Sequential()
+tf.keras.layers.Dense()
+tf.keras.layers.Dropout()
+tf.keras.layers.Flatten()
+tf.keras.layers.Concatenate()
+tf.keras.layers.Conv2D()
+tf.keras.layers.MaxPool2D()
+tf.keras.layers.MaxPooling2D()
+tf.keras.layers.AveragePooling2D()
+
+tf.keras.losses.mean_squared_error()    # 均方损失函数
+
+tf.data.Dataset.from_tensor_slices()
+tf.data.Dataset.repeat()
+tf.data.Dataset.shuffle()
+tf.data.Dataset.batch()
+tf.data.Dataset.take()
+tf.data.Dataset.map()
+tf.data.Dataset.zip()
+
+tf.keras.callbacks.EarlyStopping()
+tf.keras.callbacks.TensorBoard()
 
 x = 0
 with tf.GradientTape as tape:
@@ -17,4 +41,3 @@ with tf.GradientTape as tape:
     y = x * x 
 grad = tape.gradient(y, x)      # 求梯度，注意此处的y 必须显含第二个参数
 
-tf.keras.losses.mean_squared_error()    # 均方损失函数
