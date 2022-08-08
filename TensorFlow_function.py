e7987be7cff555ac3ae96bb5200e63314f41bd2b
Jia Xinglong <jiaxinglong1996@outlook.com>
2022-08-08 21:52:24 +0800

    增加了 losses, metrics, callbacks, summary 的一些函数

--- TensorFlow_function.py
+++ TensorFlow_function.py
@@ -1,7 +1,10 @@
 # Tensorflow的class和method
 # 鼠标放在函数名上不动，可以查看函数的输入参数和例子
 
+import tensorboard
 import tensorflow as tf 
+
+tf.enable_eager_execution()       
 
 tf.argmax()         # 找到最大值并返回索引
 tf.reduce_max()     # 求最大值
@@ -12,7 +15,18 @@ tf.one_hot()
 tf.cast()
 tf.keras.utils.to_categorical()
 
+tf.data.Dataset.from_tensor_slices()
+tf.data.Dataset.repeat()
+tf.data.Dataset.shuffle()
+tf.data.Dataset.batch()
+tf.data.Dataset.take()
+tf.data.Dataset.map()
+tf.data.Dataset.zip()
+
 tf.keras.Sequential()
+tf.keras.Sequential().trainable_weights()
+tf.keras.Sequential().trainable_variables()
+
 tf.keras.layers.Dense()
 tf.keras.layers.Dropout()
 tf.keras.layers.Flatten()
@@ -23,17 +37,26 @@ tf.keras.layers.MaxPooling2D()
 tf.keras.layers.AveragePooling2D()
 
 tf.keras.losses.mean_squared_error()    # 均方损失函数
+tf.keras.losses.BinaryCrossentropy()
+tf.keras.losses.CategoricalCrossentropy()
+tf.keras.losses.SparseCategoricalCrossentropy()
 
-tf.data.Dataset.from_tensor_slices()
-tf.data.Dataset.repeat()
-tf.data.Dataset.shuffle()
-tf.data.Dataset.batch()
-tf.data.Dataset.take()
-tf.data.Dataset.map()
-tf.data.Dataset.zip()
+tf.keras.optimizers.Adam()
+
+tf.keras.metrics.Mean()
+tf.keras.metrics.Accuracy()
+tf.keras.metrics.CategoricalAccuracy()
+tf.keras.metrics.result()
 
 tf.keras.callbacks.EarlyStopping()
 tf.keras.callbacks.TensorBoard()
+tf.keras.callbacks.LearningRateScheduler()
+
+tf.summary.scalar()
+tf.summary.create_file_writer()
+tf.summary.create_file_writer().set_as_default()
+
+# tensorboard --logdir logs
 
 x = 0
 with tf.GradientTape as tape:
